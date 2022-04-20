43e014e50a3c7a9c654b2a8bb68ad4f696f2f2fb
hamza barbara <62608203+m3ofer@users.noreply.github.com>
2022-04-20 22:37:00 +0000

    Update MqttTest.ino

--- Esp32-Mqtt-Test/MqttTest.ino
+++ Esp32-Mqtt-Test/MqttTest.ino
@@ -1,112 +1,119 @@
-#include <Arduino.h>
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include "PubSubClient.h"
 #include "DHT.h"
 #define DHTPIN 4
 #define DHTTYPE DHT22
 
-const int ledPin = 5;// red light pin
-const char *SSID = "******";//wifi name
-const char *PWD  = "******";// wifi password
+//setting up needed variables
+const char* ssid = "TP-LINK_CC2894";// wifi name
+const char* password = "comptonnitrogenpicom";// wifi password
+const char* mqttServer = "test.mosquitto.org";// mqtt server can be switched to broker.hivemq.com
+int port = 1883;
+String stMac;
+char mac[50];
+char clientId[50];
+const int ledPin = 5;
+long last_time = 0;
+char data[100];//we store data that were going to publish
 
+WiFiClient espClient;
+PubSubClient client(espClient);
 DHT dht(DHTPIN, DHTTYPE);//setting dht pins & type of dht
 
-long last_time = 0;
-char data[100];
-
-// MQTT client
-WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient);
-
-char *mqttServer = "test.mosquitto.org";//can be replaced by broker.hivemq.com
-int mqttPort = 1883;
-
-
-void connectToWiFi() {
-  Serial.print("Connecting to ");
- 
-  WiFi.begin(SSID, PWD);
-  Serial.println(SSID);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
-  Serial.print("Connected.");
-  
-} 
-//sets up call back from a topic you subscribed to
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Callback - ");
-  Serial.print("Message:");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-}
-
-void setupMQTT() {
-  mqttClient.setServer(mqttServer, mqttPort);
-  // set the callback function
-  mqttClient.setCallback(callback);
-  
-}
 
 void setup() {
-  Serial.begin(9600); 
-  connectToWiFi();
-  Serial.println(F("DHTxx test!"));
-  pinMode (ledPin, OUTPUT);
-  dht.begin();
-  setupMQTT();  
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+//Calling wifi function
+  wifiConnect();
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("MAC address: ");
+  Serial.println(WiFi.macAddress());
+  client.setServer(mqttServer, port);
+  client.setCallback(callback);
+  pinMode(ledPin, OUTPUT);
+  
 }
 
-//subs to topic commands allows to send meesages to esp32
-void reconnect() {
-  Serial.println("Connecting to MQTT Broker...");
-  while (!mqttClient.connected()) {
-      Serial.println("Reconnecting to MQTT Broker..");
-      String clientId = "ESP32Client-";
-      //clientId += String(random(0xffff), HEX);
-      
-      if (mqttClient.connect(clientId.c_str())) {
-        Serial.println("Connected.");
-        // subscribe to topic
-        mqttClient.subscribe("/swa/commands");
-      }      
+void wifiConnect() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+}
+
+void mqttReconnect() {
+  while (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    //long r = random(1000);
+    sprintf(clientId, "client-esp32");
+    if (client.connect(clientId)) {
+      Serial.print(clientId);
+      Serial.println(" connected");
+      client.subscribe("/swa/commands");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.println(topic);
+  Serial.print(". Message: ");
+  String stMessage;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    stMessage += (char)message[i];
+  }
+  Serial.println();
+  if (String(topic) == "/swa/commands") {
+    Serial.print("Changing output to ");
+    if(stMessage == "on"){
+      Serial.println("on");
+      digitalWrite(ledPin, HIGH);
+    }
+    else if(stMessage == "off"){
+      Serial.println("off");
+      digitalWrite(ledPin, LOW);
+    }
   }
 }
 
 void loop() {
-  //just testing the red led
-  digitalWrite (ledPin, HIGH);
-  delay(500);
-  digitalWrite (ledPin, LOW);
-  delay(500);
-  //checking the connection
-  if (!mqttClient.connected())
-    reconnect();
-
-  mqttClient.loop();
-
+  if (!client.connected())
+    mqttReconnect();
+    
+  client.loop();
   long now = millis();
-  if (now - last_time > 60000) {
+  if (now - last_time > 6000) {
       delay(2000);
-     
+      //Sending data to a single topic
       float t = dht.readTemperature();
       String m = "";
       m += String(WiFi.macAddress());
+    
       //checking if dht22 is regonized
       if (isnan(t)) {
-        Serial.println(F("Failed to read from DHT sensor!"));
-        return;
+         Serial.println(F("Failed to read from DHT sensor!"));
+         return;
       }
-        // Publishing data
-        sprintf(data, "{\"temp\" : \"%f\",\"person\" : \"%s\"}",t,m.c_str());
-        Serial.println(data);
-        mqttClient.publish("/swa/temperature", data);
-       Serial.println(t);
-       Serial.println(m);
-        last_time = now;
+       // Publishing data
+       sprintf(data, "{\"temp\" : \"%f\",\"person\" : \"%s\"}",t,m.c_str());
+       Serial.println(data);
+       client.publish("/swa/temperature", data);
+       last_time = now;
   }
 }
