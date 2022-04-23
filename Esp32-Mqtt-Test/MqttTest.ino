7b0a9aafd65d3d37a8a83367215d8aa7ab231aff
hamza barbara <62608203+m3ofer@users.noreply.github.com>
2022-04-23 14:51:50 +0000

    Add files via upload

--- Esp32-Mqtt-Test/MqttTest.ino
+++ Esp32-Mqtt-Test/MqttTest.ino
@@ -1,12 +1,20 @@
+// making connection to phone app
+#define BLYNK_TEMPLATE_ID "TMPLDr73x7Sw"
+#define BLYNK_DEVICE_NAME "ESPTemp32"
+#define BLYNK_AUTH_TOKEN "41IbMW7bPrialltd5BB4cZlRS71blqMQ"
+#define BLYNK_PRINT Serial
+
+//adding needed libraries
 #include <WiFi.h>
 #include "PubSubClient.h"
 #include "DHT.h"
 #define DHTPIN 4
 #define DHTTYPE DHT22
+#include <BlynkSimpleEsp32.h>
 
 //setting up needed variables
-const char* ssid = "******";// wifi name
-const char* password = "******";// wifi password
+const char* ssid = "TP-LINK_CC2894";// wifi name
+const char* password = "comptonnitrogenpicom";// wifi password
 const char* mqttServer = "test.mosquitto.org";// mqtt server can be switched to broker.hivemq.com
 int port = 1883;
 String stMac;
@@ -15,32 +23,38 @@ char clientId[50];
 const int ledPin = 5;
 long last_time = 0;
 char data[100];//we store data that were going to publish here
+char auth[] = BLYNK_AUTH_TOKEN;
+BlynkTimer timer;//blink timer basically loop for blink
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 DHT dht(DHTPIN, DHTTYPE);//setting dht pins & type of dht
 
+void sendSensor(){
+  //setting up variables
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  String m = "";
+  m += String(WiFi.macAddress());
+  //sending information for Blynk
+  Blynk.virtualWrite(V0, String(t,2));
+  Blynk.virtualWrite(V2, String(h, 2));
+  //sending information to mqtt broker
+  sprintf(data, "{\"temp\" : \"%s\",\"person\" : \"%s\"}",String(t, 2),m.c_str());
+  client.publish("/swa/temperature", data);
+  Serial.println(data);
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-//Calling wifi function
-  wifiConnect();
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.print("MAC address: ");
-  Serial.println(WiFi.macAddress());
-  client.setServer(mqttServer, port);
-  client.setCallback(callback);
-  pinMode(ledPin, OUTPUT);
-  // starting dht
-  Serial.println(F("DHTxx test!"));
-  dht.begin();
 }
-
+BLYNK_WRITE(V1){
+  //int pinValue= param.asInt();
+  //digitalWrite(led,pinValue);
+   if (param.asInt()) {
+     client.publish("temp","on");
+   }
+   else {
+     client.publish("temp","off");
+   }
+}
 void wifiConnect() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -84,12 +98,37 @@ void callback(char* topic, byte* message, unsigned int length) {
     if(stMessage == "on"){
       Serial.println("on");
       digitalWrite(ledPin, HIGH);
+      Blynk.virtualWrite(V1,1);
     }
     else if(stMessage == "off"){
       Serial.println("off");
       digitalWrite(ledPin, LOW);
+      Blynk.virtualWrite(V1,0);
     }
   }
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+//Calling wifi function
+  wifiConnect();
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("MAC address: ");
+  Serial.println(WiFi.macAddress());
+  client.setServer(mqttServer, port);
+  client.setCallback(callback);
+  pinMode(ledPin, OUTPUT);
+  // starting dht
+  Serial.println(F("DHTxx test!"));
+  dht.begin();
+  //starting blink & blynk timer
+  Blynk.begin(auth, ssid, password);
+  timer.setInterval(100L, sendSensor);
 }
 
 void loop() {
@@ -97,14 +136,16 @@ void loop() {
     mqttReconnect();
     
   client.loop();
-  long now = millis();
+  Blynk.run();
+  timer.run();
+  /*long now = millis();
   if (now - last_time > 6000) {
       delay(2000);
       //Sending data to a single topic
       float t = dht.readTemperature();
+      //float h = dht.readHumidity();
       String m = "";
       m += String(WiFi.macAddress());
-    
       //checking if dht22 is regonized
       if (isnan(t)) {
          Serial.println(F("Failed to read from DHT sensor!"));
@@ -114,6 +155,9 @@ void loop() {
        sprintf(data, "{\"temp\" : \"%f\",\"person\" : \"%s\"}",t,m.c_str());
        Serial.println(data);
        client.publish("/swa/temperature", data);
+       //Serial.print("Temperature : ");
+       //Serial.print(t,2);
+       //Serial.println("C°");
        last_time = now;
-  }
+  }*/
 }
